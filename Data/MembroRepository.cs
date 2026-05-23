366c8253184155162378589b2cb08e751adf2ed6
Rafael <rafaelzaap.dev@gmail.com>
2026-05-23 09:16:21 -0300

    correção modo debug

--- Data/MembroRepository.cs
+++ Data/MembroRepository.cs
@@ -28,8 +28,7 @@ public class MembroRepository : IMembroRepository
                 primeiro_nome AS PrimeiroNome,
                 ativo AS Ativo
             FROM membros
-            WHERE CODIGO = 37
-              AND (@IncluirInativos = TRUE OR ativo = 1)
+            WHERE (@IncluirInativos = TRUE OR ativo = 1)
               AND (
                     @Busca IS NULL
                     OR nome LIKE CONCAT('%', @Busca, '%')
